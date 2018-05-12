254a062e091659c4fdac58974f8df1f523a5a7e0
juliahener <jf.hener@yahoo.de>
2018-05-12 02:09:15 +0200

    derive attributes for province entities

--- ntmeta/systems/province.py
+++ ntmeta/systems/province.py
@@ -2,11 +2,35 @@
 import os
 from django.conf import settings
 
+from ntmeta.choices import QualityTypeChoices
 from ntmeta.models import Component
 
 COMPONENTS = ('Province', )
 
 GAME_MODEL = 'Province'
+
+DEFAULT_KWARGS_MAP = {
+    QualityTypeChoices[0][0]: {
+        'max_length': 100,
+        'null': True,
+        'blank': True,
+    },
+    QualityTypeChoices[1][0]: {
+        'default': 0,
+    },
+    QualityTypeChoices[2][0]: {
+        'default': 0.0,
+    },
+    QualityTypeChoices[3][0]: {
+        'blank': False,
+        'default': True,
+    }
+    QualityTypeChoices[4][0]: {}
+}
+
+DEFAULT_ARGS_MAP = {
+    QualityTypeChoices[4][0]: {"'GoldCoin'"}
+}
 
 ENTITY_MODEL_TEMPLATE = """
 from django.db import models
@@ -18,7 +42,7 @@ class %(name)s(models.Model):
 """
 
 QUALITY_TEMPLATE = """
-    %(label)s = %(field)s(%(kwargs)s)
+    %(label)s = %(field)s(%(args)s, %(kwargs)s)
 """
 
 class ProvinceSystem(object):
@@ -40,7 +64,25 @@ class ProvinceSystem(object):
         for e in entities:
             print(e.id)
             # get aspect qualities
-            print(e.aspect_set.all())
+            a_set = e.aspect_set.all()
+            for a in a_set:
+                # create an attribute for each quality
+                for q in a.qualities.all():
+                    label = a.name.lower() if (
+                        a.name == q.name) else '%s_%s' % (a.name, q.name)
+                    print(label)
+                    field = q.data_type
+                    args = DEFAULT_ARGS_MAP.get(field)
+                    kwargs = DEFAULT_KWARGS_MAP.get(field)
+
+                    attributes.append(QUALITY_TEMPLATE % {
+                        'label': label,
+                        'field': field,
+                        'args': *args,
+                        'kwargs': **kwargs,
+                    })
+
+        print(attributes)
 
         # with open(GAME_MODEL.lower()+'.py', 'w') as model:
         # build attribute string from aspect qualities (+ \n)
@@ -53,5 +95,5 @@ class ProvinceSystem(object):
         #
         # for any existing instances of the releated GAMEMODEL, create an instance of
         # model and attach
-        
+
         print('Done!')  # TODO real logging output
