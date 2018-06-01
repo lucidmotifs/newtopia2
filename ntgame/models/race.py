832ea86b139e22ae5001b20540abce1225c04512
Paul Cooper <p.cooper@mac.com>
2018-06-01 10:15:04 +0200

    added max length

--- ntgame/models/race.py
+++ ntgame/models/race.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Race(models.Model):
 
-    name = models.CharField(default='Human')
+    name = models.CharField(max_length=100, default='Human')
 
     class Meta:
         verbose_name_plural = 'Races'
