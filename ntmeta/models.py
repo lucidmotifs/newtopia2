bfc0de5b16e47d5fbc0487c5d98d782f77c916c8
Paul Cooper <p.cooper@mac.com>
2018-06-01 10:15:58 +0200

    attach templates as properties

--- ntmeta/models.py
+++ ntmeta/models.py
@@ -2,26 +2,27 @@ from django.db import models
 
 from .choices import QualityTypeChoices
 
-# Create your models here.
+
 class Entity(models.Model):
+    CLASS_TEMPLATE = \
+    """
+    class {name}(models.Model):
+        entity = models.ForeignKey(
+            'ntmeta.Entity', on_delete=models.CASCADE, default={id})
+        {attributes}
+    """
+
+    # django fields
     name = models.CharField(max_length=100, unique=True)
 
     def gen_game_model(self):
         # @TODO move to strings/templates module
-        template = \
-        """
-        class %(name)s(models.Model):
-            entity = models.ForeignKey(
-                'ntmeta.Entity', on_delete=models.CASCADE, default=%(id)s)
-        
-            %(attributes)s
-        """
 
         attr_fields = []
         for a in self.aspect_set.all():
             attr_fields.append(q.gen_attr_field() for q in a.quality_set.all())
 
-        return template % ({
+        return Entity.CLASS_TEMPLATE % ({
             'name': self.name,
             'id': self.id,
             'attributes': '\n'.join(attr_fields),
@@ -43,16 +44,20 @@ class Aspect(models.Model):
 
 
 class Quality(models.Model):
+    FIELD_TEMPLATE = "%(label)s = %(field)s(%(args)s%(kwargs)s)"
+    # should be loaded from QualityFieldArgs (args) when done
+    max_length = 255
+
     label = models.CharField(max_length=50, unique=False)
     data_type = models.CharField(choices=QualityTypeChoices, max_length=20)
+    description = models.CharField(max_length=255, default='No Description')
+    nullable = models.BooleanField(default=False)
+    blank = models.BooleanField(default=False)
+
+
+    # relationships
     aspect = models.ForeignKey(
         Aspect, blank=True, on_delete=models.CASCADE, default=1)
-
-
-    def gen_attr_field(self):
-        template = '%(label)s = %(field)s(%(args)s%(kwargs)s)'
-
-        
 
     class Meta:
         verbose_name_plural = 'Qualities'
