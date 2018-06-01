b05ea52639c75e2f7e21f3dfda1898a05d44bc9b
Paul Cooper <p.cooper@mac.com>
2018-06-01 10:15:19 +0200

    use enum for values of choices

--- ntmeta/choices.py
+++ ntmeta/choices.py
@@ -1,11 +1,21 @@
+from enum import Enum
+
 from django.db.models import fields
 from django.db.models.fields.related import ForeignKey
 
+# TODO move ot seperate module
+class QualityType(Enum):
+    TEXT = 'Text'
+    NUMBER = 'Number'
+    FLOAT = 'Float'
+    BOOL = 'Bool'
+    ENTITY = 'Entity'
+
 
 QualityTypeChoices = (
-    (fields.CharField.__name__, 'Text'),
-    (fields.IntegerField.__name__, 'Number'),
-    (fields.FloatField.__name__, 'Float'),
-    (fields.BooleanField.__name__, 'Bool'),
-    (ForeignKey.__name__, 'Entity'),
+    (fields.CharField.__name__, QualityType.TEXT),
+    (fields.IntegerField.__name__, QualityType.NUMBER),
+    (fields.FloatField.__name__, QualityType.FLOAT),
+    (fields.BooleanField.__name__, QualityType.BOOL),
+    (ForeignKey.__name__, QualityType.ENTITY),
 )
